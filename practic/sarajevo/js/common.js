2d31be955c7bc05a2e7b58eaf5b45789c3c8a865
sydney <o.bova@meta.ua>
2017-09-24 16:34:49 +0300

    правки

--- practic/sarajevo/js/common.js
+++ practic/sarajevo/js/common.js
@@ -2,11 +2,45 @@ $(function () {
   $('.js-form').validate();
 });
 
+$(document).ready(function(){
+  $("#menu").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 500);
+  });
+});
+
 $(document).ready(function() {
   $('.team-content').slick({
     slidesToShow: 4,
     slidesToScroll: 2,
-    arrows: true
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 860,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 })
 
